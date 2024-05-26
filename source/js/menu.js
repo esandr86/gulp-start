14ad4c8a9821877ab7d2a1934ce5766da004a024
Aleksandr <163760775+esandr86@users.noreply.github.com>
2024-05-27 00:33:41 +0500

    Внес корректировки по коду, добавил overlay

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,18 @@
 const headerOpenMenu = document.querySelector('.header__menu')
 const headerContainer = document.querySelector('.header__container')
 const hederCloseMenu = document.querySelector('.header__close')
+const overlay = document.querySelector('.overlay');
 
 headerOpenMenu.addEventListener('click', () => {
   headerContainer.classList.add('header__container--showed');
+  overlay.classList.add('overlay--showed');
   hederCloseMenu.addEventListener('click', headerCloseButton);
+  overlay.addEventListener('click', headerCloseButton);
 });
 
 const headerCloseButton = () => {
   headerContainer.classList.remove('header__container--showed');
+  overlay.classList.remove('overlay--showed');
   hederCloseMenu.removeEventListener('click', headerCloseButton);
+  overlay.removeEventListener('click', headerCloseButton);
 };
--- source/js/modal.js
+++ source/js/modal.js
@@ -2,18 +2,22 @@ const productButton = document.querySelectorAll('.product__button');
 const modalProduct = document.querySelector('.modal__product');
 const modalProductClose = document.querySelector('.modal__close');
 const modalContinueShopping = document.querySelector('.modal__button');
+const modalOverlay = document.querySelector('.overlay');
 
 const modalClose = () => {
   modalProduct.classList.remove('modal__product--showed');
+  modalOverlay.classList.remove('overlay--showed');
   modalProductClose.removeEventListener('click', modalClose);
+  modalContinueShopping.removeEventListener('click', modalClose);
+  modalOverlay.removeEventListener('click', headerCloseButton);
 };
-
-const modalContinueButton = modalClose;
 
 function openModal () {
   modalProduct.classList.add('modal__product--showed');
+  modalOverlay.classList.add('overlay--showed');
   modalProductClose.addEventListener('click', modalClose);
-  modalContinueShopping.addEventListener('click', modalContinueButton);
+  modalContinueShopping.addEventListener('click', modalClose);
+  modalOverlay.addEventListener('click', headerCloseButton);
 }
 
 productButton.forEach(function(productButton){
